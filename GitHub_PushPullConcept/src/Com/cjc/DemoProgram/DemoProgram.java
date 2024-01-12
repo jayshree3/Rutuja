d671f34f7368e63cb42c85e74d4bd358ba7c7fdc
HP <HP@LAPTOP-KUH9GEJ5>
2024-01-12 10:17:53 +0530

    By Jayshree

---
+++ GitHub_PushPullConcept/src/Com/cjc/DemoProgram/DemoProgram.java
@@ -0,0 +1,27 @@
+package Com.cjc.DemoProgram;
+
+public class DemoProgram 
+{
+	
+	public void m1()
+	{
+		System.out.println("M1----A");
+	}
+	public int m2( int a,int b)
+	{
+		int c=a+b;
+		return  c;
+	}
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Main Start");
+		DemoProgram d=new DemoProgram();
+		d.m1();
+		int c=d.m2(90, 120);
+		System.out.println(c);
+		
+		
+	}
+
+}
